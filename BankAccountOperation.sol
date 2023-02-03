34fae6cbb716169c848d77eb16eb1e68ccf87d86
Willy2017 <kiry226@gmail.com>
2023-02-03 16:21:52 +0800

    commit 202302031640

--- BankAccountOperation.sol
+++ BankAccountOperation.sol
@@ -4,26 +4,50 @@ pragma solidity >=0.7.0 <0.9.0;
 import "hardhat/console.sol";
 
 contract BankAccountOperation {
-    address public owner;
-    modifier onlyWhiteList() {
+    address private owner;
+    mapping(address=> bool) private depositWhiteList;
+    mapping(address=> bool) private withdrawWhiteList;
+
+    modifier onlyOwner() {
         require(owner == msg.sender,"only owner can do this!");
+        _;
+    }
+
+    modifier onlyDepositWhiteList() {
+        require(depositWhiteList[msg.sender] == true,"only deposit white list can do this!");
+        _;
+    }
+
+    modifier onlyWithdrawWhiteList() {
+        require(withdrawWhiteList[msg.sender] == true,"only withdraw white list can do this!");
         _;
     }
     constructor() payable {
         owner=msg.sender;
+        depositWhiteList[owner] = true;
+        withdrawWhiteList[owner] = true;
     }
     receive() payable external{}
 
-    function withdraw() onlyWhiteList external {
+    function withdraw() onlyWithdrawWhiteList external {
         address payable Receiver = payable(msg.sender);
         Receiver.transfer(address(this).balance);
-    }
-
-    function deposit() payable onlyWhiteList external {
         console.log(address(this).balance);
     }
 
-    function gerBalance() external view {
+    function deposit() payable onlyDepositWhiteList external {
         console.log(address(this).balance);
+    }
+
+    function getBalance() external view {
+        console.log(address(this).balance);
+    }
+
+    function addDepositWhiteList(address depositWhiteAddress) onlyOwner external {
+        depositWhiteList[depositWhiteAddress] = true;
+    }
+
+    function addWithdrawWhiteList(address withdrawWhiteAddress) onlyOwner external {
+        withdrawWhiteList[withdrawWhiteAddress] = true;
     }
 }
