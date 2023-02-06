a2451ccb23497658e213d884a1aac37d15513a91
Willy2017 <kiry226@gmail.com>
2023-02-06 14:04:32 +0800

    commit 202302061403

--- BankAccountOperation.sol
+++ BankAccountOperation.sol
@@ -5,37 +5,25 @@ import "hardhat/console.sol";
 
 contract BankAccountOperation {
     address private owner;
-    mapping(address=> bool) private depositWhiteList;
-    mapping(address=> bool) private withdrawWhiteList;
+    mapping(address=> uint) private balanceList;
 
-    modifier onlyOwner() {
-        require(owner == msg.sender,"only owner can do this!");
-        _;
-    }
-
-    modifier onlyDepositWhiteList() {
-        require(depositWhiteList[msg.sender] == true,"only deposit white list can do this!");
-        _;
-    }
-
-    modifier onlyWithdrawWhiteList() {
-        require(withdrawWhiteList[msg.sender] == true,"only withdraw white list can do this!");
-        _;
-    }
     constructor() payable {
+        balanceList[msg.sender] += msg.value;
         owner=msg.sender;
-        depositWhiteList[owner] = true;
-        withdrawWhiteList[owner] = true;
     }
-    receive() payable external{}
 
-    function withdraw() onlyWithdrawWhiteList external {
+    function withdraw(uint amount) external {
         address payable Receiver = payable(msg.sender);
-        Receiver.transfer(address(this).balance);
-        console.log(address(this).balance);
+        if (balanceList[msg.sender] >= amount) {
+            Receiver.transfer(amount);
+            balanceList[msg.sender] -= amount;
+        }
+
+        console.log(balanceList[msg.sender]);
     }
 
-    function deposit() payable onlyDepositWhiteList external {
+    function deposit() payable external {
+        balanceList[msg.sender] += msg.value;
         console.log(address(this).balance);
     }
 
@@ -43,11 +31,4 @@ contract BankAccountOperation {
         console.log(address(this).balance);
     }
 
-    function addDepositWhiteList(address depositWhiteAddress) onlyOwner external {
-        depositWhiteList[depositWhiteAddress] = true;
-    }
-
-    function addWithdrawWhiteList(address withdrawWhiteAddress) onlyOwner external {
-        withdrawWhiteList[withdrawWhiteAddress] = true;
-    }
 }
