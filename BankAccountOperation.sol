577ba41ace3f2b466192fca6f510ca3472c3a213
Willy2017 <kiry226@gmail.com>
2023-02-06 15:22:32 +0800

    commit 202302061522

--- BankAccountOperation.sol
+++ BankAccountOperation.sol
@@ -5,25 +5,59 @@ import "hardhat/console.sol";
 
 contract BankAccountOperation {
     address private owner;
-    mapping(address=> uint) private balanceList;
+
+    struct addressInfo {
+        bool state;
+        uint pos;
+    }
+
+    mapping(address=> addressInfo) private addressPosList;
+
+    struct accountInfo {
+        address _address;
+        uint balance;
+    }
+
+    accountInfo[] private accountList;
 
     constructor() payable {
-        balanceList[msg.sender] += msg.value;
+        accountInfo memory a;
+        a._address = msg.sender;
+        a.balance += msg.value;
+        accountList.push(a);
+        addressPosList[msg.sender].pos = accountList.length - 1;
+        addressPosList[msg.sender].state = true;
         owner=msg.sender;
     }
 
-    function withdraw(uint amount) external {
-        address payable Receiver = payable(msg.sender);
-        if (balanceList[msg.sender] >= amount) {
-            Receiver.transfer(amount);
-            balanceList[msg.sender] -= amount;
-        }
+    function withdraw(uint amount) payable external returns (bool){
 
-        console.log(balanceList[msg.sender]);
+        if (addressPosList[msg.sender].state) {
+            if (accountList[addressPosList[msg.sender].pos].balance < amount) {
+                return false;
+            }
+
+            address payable Receiver = payable(msg.sender);
+            accountList[addressPosList[msg.sender].pos].balance -= amount;
+            Receiver.transfer(amount);
+            console.log(address(this).balance);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     function deposit() payable external {
-        balanceList[msg.sender] += msg.value;
+        if (addressPosList[msg.sender].state) {
+            accountList[addressPosList[msg.sender].pos].balance += msg.value;
+        } else {
+            accountInfo memory a;
+            a._address = msg.sender;
+            a.balance += msg.value;
+            accountList.push(a);
+            addressPosList[msg.sender].pos = accountList.length - 1;
+            addressPosList[msg.sender].state = true;
+        }
         console.log(address(this).balance);
     }
 
@@ -31,4 +65,15 @@ contract BankAccountOperation {
         console.log(address(this).balance);
     }
 
+    function destroySmartContract() public {
+        require(msg.sender == owner, "You are not the owner!");
+        selfdestruct(payable(owner));
+    }
+
+    function printAll() public view {
+        for(uint i=0;i<accountList.length;i++) {
+            console.log(accountList[i]._address);
+            console.log(accountList[i].balance);
+        }
+    }
 }
